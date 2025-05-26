d0afb572d315ecf7336196f83b2b22385341cab1
mayaraaguiar <mayara.aguiar@sptech.school>
2025-05-26 18:49:35 -0300

    Adicionado tabela BD com select e atualizado DER

--- MIO-web-data-viz/src/database/script-tabelas.sql
+++ MIO-web-data-viz/src/database/script-tabelas.sql
@@ -1,62 +1,131 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+drop database PI;
+create database PI;
+use PI;
 
-/*
-comandos para mysql server
-*/
-drop database aquatech;
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
+create table profissional(
+idProfissional int auto_increment primary key,
+nomeProfissional varchar(45),
+email varchar(45),
+senha varchar (45),
+codigoAtivacao VARCHAR(7)
 );
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+
+create table gestante(
+idGestante int auto_increment primary key,
+nomeGestante varchar(45), 
+dtNascimento varchar(45),
+telefone varchar (11)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+create table infoGestante(
+idInfoGestante int primary key auto_increment,
+fkGestante int,
+statusPreNatal varchar (7),
+gesta decimal,
+paridade decimal,
+aborto decimal,
+numPN decimal,
+numPC decimal,
+dum date,
+dpp date,
+altoRisco varchar(3),
+constraint chkAltoRisco
+		check (altoRisco in ('Sim', 'Não')),
+constraint chkStatus
+		check (statusPreNatal in ('Ativo', 'Inativo')),
+constraint fk_InfoGestante foreign key (fkGestante)
+		references gestante (idGestante));
+
+create table consulta(
+idConsulta int, 
+fkGestanteAssociativa int,
+fkProfissionalAssociativa int,
+au decimal,
+bcf decimal,
+pressaoArt varchar (6),
+mf varchar(3),
+constraint chkMf
+ check (mf in ('Sim','Não')),
+primary key (idConsulta, fkGestanteAssociativa, fkProfissionalAssociativa)
+ );
+ 
+create table historicoPreNatal(
+idFinalizado int primary key auto_increment,
+viaParto varchar (45),
+complicacoes varchar (3),
+rnVivo varchar (3),
+dtParto date,
+fkGestanteHistorico int,
+constraint chkViaParto
+		check (viaParto in ('Normal', 'Cesárea')),
+constraint chkComplicacoes
+		check (complicacoes in ('Sim', 'Não')),
+constraint chkRnVivo
+		check (rnVivo in ('Sim', 'Não'))
 );
+ 
+ 
+ insert into profissional values
+ ( default, 'Samia', 'samia@email.com', 'Zamj123', 'c20bje1'),
+( default, 'Aline', 'aline@email.com', 'Zamj124', 'c20bje2'),
+( default, 'Beatriz', 'beatriz@email.com', 'Zamj125', 'c20bje2');
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+insert into gestante values
+(default, 'Thamires', '1996-11-25', '11959477652'),
+(default, 'Juliana', '1990-05-02', '11959477656'),
+(default, 'Leidiane', '1996-12-31', '11959477682');
 
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
+insert into infoGestante values
+(default, 1, 'Ativo', 1, 0, 0, 0, 0, '2024-03-15', '2025-11-25', 'Sim'),
+(default, 2, 'Ativo', 3, 1, 1, 0, 1, '2025-02-09', '2025-02-09', 'Sim'),
+(default, 3, 'Ativo', 1, 0, 1, 0, 0, '2025-04-06', '2025-08-16', 'Não');
 
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+insert into historicoPreNatal values 
+(default, 'Normal', 'Não', 'Sim', '2025-04-15', 1),
+(default, 'Cesárea', 'Sim', 'Sim', '2025-02-08', 2),
+(default, 'Normal', 'Sim', 'Sim', '2025-02-04', 2),
+(default, 'Normal', 'Não', 'Sim', '2025-01-05', 3),
+(default, 'Cesárea', 'Sim', 'Sim', '2025-03-22', 1);
+
+ -- Exibir pacientes que estão realizando pré natal ativas (KPI) 
+SELECT COUNT(*) AS totalGestantesAtivas
+FROM infoGestante
+WHERE statusPreNatal = 'Ativo';
+
+ -- Gestantes que estão realizando pré natal de de alto risco (KPI)
+ SELECT count(*) as nomeGestante 
+FROM gestante g
+JOIN infoGestante i ON g.idGestante = i.fkGestante
+WHERE i.altoRisco = 'Sim';
+
+ -- Gestantes que tiveram complicações no ultimo ano (10/60) Total e complicações.. 7. Total de partos com e sem complicações
+SELECT COUNT(*) AS totalComComplicacoes
+FROM historicoPreNatal
+WHERE complicacoes = 'Sim'
+  AND dtParto >= '2024-05-24';
+
+SELECT COUNT(*) AS totalSemComplicacoes
+FROM historicoPreNatal
+WHERE complicacoes = 'Não'
+  AND dtParto >= '2024-05-24';
+
+
+ 
+ -- Gestante com DPP para cada mês do ano 
+ SELECT MONTH(dpp) AS mes, COUNT(*) AS total
+FROM infoGestante
+GROUP BY MONTH(dpp)
+ORDER BY mes;
+ -- Via de parto no ultimo ano 
+ 
+SELECT viaParto, COUNT(*) AS total
+FROM historicoPreNatal
+GROUP BY viaParto;
+
+--  6. Total de RN vivos vs óbitos
+SELECT rnVivo, COUNT(*) AS total
+FROM historicoPreNatal
+GROUP BY rnVivo;
+
