e4288e551f2704992e01787d9437c0b133f2985b
mayaraaguiar <mayara.aguiar@sptech.school>
2025-06-04 20:29:08 -0300

    Adicionado input tela consulta

--- MIO-web-data-viz/app.js
+++ MIO-web-data-viz/app.js
@@ -18,6 +18,7 @@ var app = express();
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var gestanteRouter = require("./src/routes/gestante");
+var dashRouter = require("./src/routes/dash");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,6 +29,7 @@ app.use(cors());
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/gestante", gestanteRouter);
+app.use("/dash", dashRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
